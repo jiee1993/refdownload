1cfd950e7ebfd997e4ba0eb83fc36eeb3a5e2f0c
Michael Hall <mbhall88@gmail.com>
2018-02-14 11:08:12 +0000

    inital commit

---
+++ refdownload/refdownload.py
@@ -0,0 +1,34 @@
+""""This is a program to download reference fasta files from NCBI using their
+accession number(s). The fasta file(s) will be either written to file or, in
+the case of a single file, written to stdout.
+
+Usage:
+
+    python3 refdownload.py <acc_num1> [<acc_num2> ...] -o /path/to/save/files/
+
+Files are saved as `acc_num.fa`.
+
+Examples:
+    # download M. bovis reference to stdout
+    python3 refdownload.py NC_002945.4
+
+    # download M. bovis and E. coli references
+    python3 refdownload.py NC_002945.4 U00096.3 -o Data/References/
+
+Please see the GitHub page for more information
+https://github.com/mbhall88/refdownload/
+
+Contributors:
+Michael Hall (https://github.com/mbhall88)
+"""
+import requests
+import refdownload.arguments as arguments
+
+
+def main():
+    """Main function to coordinate the running of the script"""
+    args = arguments.generate_args(__doc__)
+
+
+if __name__ == '__main__':
+    main()

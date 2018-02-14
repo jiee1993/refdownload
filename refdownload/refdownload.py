6fc435f8d2532104f9ae96744e1091ff7ed5ad5e
Michael Hall <mbhall88@gmail.com>
2018-02-14 13:04:18 +0000

    add raw code to be formalised

--- refdownload/refdownload.py
+++ refdownload/refdownload.py
@@ -29,6 +29,23 @@ import requests
 import arguments
 
 
+"""raw code to download
+import requests
+acc = "NC_002945.4"
+base = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+url = base + "esearch.fcgi?db=nuccore&term={}&usehistory=y".format(acc)
+result = requests.get(url)
+result.text
+# grab the below bit out of the <WebEnv> xml tags
+web = "NCID_1_315897282_130.14.22.215_9001_1518599921_1103742014_0MetA0_S_MegaStore_F_1"
+# grab the below bit out of the <QueryKey> xml tag (regex for digit)
+key = 1
+url2 = base + "efetch.fcgi?db=nuccore&query_key={}&WebEnv={}".format(key, web) + "&rettype=fasta&retmode=text"
+fasta = requests.get(url2)
+fasta.text
+
+"""
+
 def main():
     """Main function to coordinate the running of the script"""
     args = arguments.generate_args(__doc__)
